46b4fb439ac527e76ea87815ab39dc7e9c35438f
Bryce McNab <bmcnab@toolbox2.osde.enops.net>
2017-10-30 15:35:29 -0700

    SYS-1674 ; Removed ticket_count import, added load metric file unittest
    Change-Id: Ifa4dbb822d7c43d1862f501449265b2772ae8db4

--- unittest_jiradog.py
+++ unittest_jiradog.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import unittest
+import json
 from jiradog import mean_time_between_statuses
-from jiradog import ticket_count
+from jiradog import load_metric_file
 
 class JiradogTestCase(unittest.TestCase):
     """Testing for `jiradog.py`"""
@@ -14,6 +15,20 @@ class JiradogTestCase(unittest.TestCase):
             expected True
         """
         self.assertEqual(mean_time_between_statuses('2017-10-25T12:00:00', '2017-10-30T12:00:00'), 5)
+
+    def test_load_metric_file(self):
+        """Test if diven json loads
+
+        Returns:
+            expected True
+        """
+        CONFIG_FILE = 'config.json'
+        with open(CONFIG_FILE) as config_data_file:
+            CONFIG_DATA_LOADED = json.load(config_data_file)
+        METRIC_FILE = CONFIG_DATA_LOADED['local']['metric_file']
+        with open(METRIC_FILE) as metric_data_file:
+            METRIC_DATA_LOADED = json.load(metric_data_file)
+        self.assertEqual(load_metric_file(METRIC_FILE, True), METRIC_DATA_LOADED)
 
 if __name__ == '__main__':
     unittest.main()
