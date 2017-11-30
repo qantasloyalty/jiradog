6c0508ae7805f340bb2b78f2d9b96d32ade289b1
Bryce McNab <bmcnab@toolbox2.osde.enops.net>
2017-11-30 14:53:01 -0800

    created build script
    Change-Id: I8310fad0627e0669878230c1df11ae49b013c7ad

--- README.md
+++ README.md
@@ -199,7 +199,7 @@ jiradog follows the [SemVer](https://semver.org/spec/v2.0.0.html) method of soft
 >
 > - MAJOR version when you make incompatible API changes,
 > - MINOR version when you add functionality in a backwards-compatible manner, and
-> - PATCH version when you make backwards-compatible bug fixes.m
+> - PATCH version when you make backwards-compatible bug fixes.
 >
 > Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format. (Preston-Warner, 2013)
 
--- unittest_jiradog.py
+++ unittest_jiradog.py
@@ -9,6 +9,7 @@ from random import randint
 from jiradog import mean_time_between_statuses
 from jiradog import load_metric_file
 from jiradog import pretty_date
+from jiradog import custom_field_sum
 
 class JiradogTestCase(unittest.TestCase):
     """Testing for `jiradog.py`"""
@@ -50,9 +51,8 @@ class JiradogTestCase(unittest.TestCase):
             'updated': NOW
             }
         }
-        if MORALS < 7:
+        if MORALS <= 7:
             DAYS_TO_CHECK = DAYS_TO_SUBTRACT
-            print mean_time_between_statuses(METRIC_DATA_LOADED, POSITION, ISSUE)
             self.assertEqual(mean_time_between_statuses(METRIC_DATA_LOADED, POSITION, ISSUE),
                              DAYS_TO_CHECK)
         else:
@@ -75,6 +75,62 @@ class JiradogTestCase(unittest.TestCase):
         with open(METRIC_FILE) as metric_data_file:
             METRIC_DATA_LOADED = json.load(metric_data_file)
         self.assertEqual(load_metric_file(METRIC_FILE, False), METRIC_DATA_LOADED)
+
+    def test_custom_field_sum(self):
+        """Test if given custom fields are added properly.
+
+        Returns:
+            expected True ~80%, False ~20%
+        """
+        class pretend_jira_object(object):
+            pass
+
+        CUSTOM_FIELD = 'custom_field_0'
+        MORALS = randint(0, 9)
+        CUSTOM_FIELD_VALUES = [
+            randint(0, 99),
+            randint(0, 99),
+            randint(0, 99),
+            randint(0, 99),
+            randint(0, 99)
+        ]
+
+        ISSUE_ONE = pretend_jira_object()
+        ISSUE_ONE.fields = pretend_jira_object()
+        ISSUE_ONE.fields.custom_field_0 = CUSTOM_FIELD_VALUES[0]
+
+        ISSUE_TWO = pretend_jira_object()
+        ISSUE_TWO.fields = pretend_jira_object()
+        ISSUE_TWO.fields.custom_field_0 = CUSTOM_FIELD_VALUES[1]
+
+        ISSUE_THR = pretend_jira_object()
+        ISSUE_THR.fields = pretend_jira_object()
+        ISSUE_THR.fields.custom_field_0 = CUSTOM_FIELD_VALUES[2]
+
+        ISSUE_FOU = pretend_jira_object()
+        ISSUE_FOU.fields = pretend_jira_object()
+        ISSUE_FOU.fields.custom_field_0 = CUSTOM_FIELD_VALUES[3]
+
+        ISSUE_FIV = pretend_jira_object()
+        ISSUE_FIV.fields = pretend_jira_object()
+        ISSUE_FIV.fields.custom_field_0 = CUSTOM_FIELD_VALUES[4]
+
+        ISSUES = [
+            ISSUE_ONE,
+            ISSUE_TWO,
+            ISSUE_THR,
+            ISSUE_FOU,
+            ISSUE_FIV
+        ]
+
+        if MORALS <= 7:
+            CUSTOM_FIELD_SUM = sum(CUSTOM_FIELD_VALUES)
+            self.assertEqual(custom_field_sum(ISSUES, CUSTOM_FIELD), CUSTOM_FIELD_SUM)
+        else:
+            CUSTOM_FIELD_SUM = randint(0,99)
+            while CUSTOM_FIELD_SUM == sum(CUSTOM_FIELD_VALUES):
+                CUSTOM_FIELD_SUM = randint(0,99)
+            self.assertNotEqual(custom_field_sum(ISSUES, CUSTOM_FIELD), CUSTOM_FIELD_SUM)
 
 if __name__ == '__main__':
     unittest.main()
